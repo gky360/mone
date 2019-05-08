66242064b7f38cc8b07ad280dfcd070827c3b809
gky360 <gky360@gmail.com>
2019-05-08 19:04:02 +0900

    use pnet instead of pcap

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,40 @@
-use pcap::{Capture, Device};
+use pnet::datalink;
+use pnet::datalink::{Channel::Ethernet, NetworkInterface};
+use std::error;
 
-type Error = pcap::Error;
+type Error = Box<error::Error>;
 
 pub fn run() -> Result<(), Error> {
-    let main_device = Device {
-        // name: String::from("enp4s0"),
-        name: String::from("en8"),
-        desc: None,
+    let interface_name = "en8";
+    let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
+
+    // Find the network interface with the provided name
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap();
+
+    // Create a new channel, dealing with layer 2 packets
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unhandled channel type"),
+        Err(e) => panic!(
+            "An error occurred when creating the datalink channel: {}",
+            e
+        ),
     };
-    println!("{:#?}", main_device);
-    let mut cap = Capture::from_device(main_device)?
-        .promisc(true)
-        .snaplen(0)
-        .open()?;
 
     let mut total = 0;
-    while let Ok(packet) = cap.next() {
-        total += packet.len();
-        println!("len: {:>10} B, total: {:>10} B", packet.len(), total);
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                let len = packet.len();
+                total += len;
+                println!("len: {:>10} B, total: {:>10} B", len, total);
+            }
+            Err(err) => return Err(Box::new(err)),
+        }
     }
-
-    Ok(())
 }
